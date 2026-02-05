51f44dfea4b0d070a304f12f102f2b121ca90c5c
Harii55 <hariii5389@gmail.com>
2026-02-05 15:58:06 +0530

    prog: implement response streaming server and fix import in files

---
+++ Response-streaming/server.py
@@ -0,0 +1,39 @@
+import asyncio
+import grpc
+import os
+import sys
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+class HelloWorldStreamingServicer(helloworld_pb2_grpc.GreeterServicer):
+    
+    async def SayHelloStreamReply(self, request, context: grpc.aio.ServicerContext):
+        print(f"Received request from: {request.name}")
+        for i in range(5):
+            await asyncio.sleep(1)  # Simulate some delay
+            yield helloworld_pb2.HelloReply(
+                message=f"Hello, {request.name}! This is message {i+1}.")
+            
+
+
+async def server():
+    port="50051"
+    server=grpc.aio.server()
+
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(HelloWorldStreamingServicer(),server)
+
+    server.add_insecure_port(f'[::]:{port}')
+    await server.start()
+
+    print(f"Responce Streaming Server started, listening on {port}")
+
+    await server.wait_for_termination()
+
+if __name__ == "__main__":
+    asyncio.run(server())
---
+++ Simple RPC/server.py
@@ -0,0 +1,45 @@
+import os
+import sys
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+import helloworld_pb2
+import helloworld_pb2_grpc
+import grpc
+from concurrent import futures
+import asyncio
+
+class HelloWorldServicer(helloworld_pb2_grpc.GreeterServicer):
+   
+    async def SayHello(self, request, context: grpc.aio.ServicerContext):
+
+        # client will send a request with a name, and we will print it on the server side, and send hello name back to client.
+        print(f"Received request from: {request.name}")
+
+        
+        return helloworld_pb2.HelloReply(
+            message=f"Hello, {request.name}!")
+    
+
+
+async def serve():
+    # create a grpc server
+    port="50051"
+    server=grpc.aio.server()  
+
+    # register the servicer
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(HelloWorldServicer(), server)
+
+    # bind to port
+    server.add_insecure_port(f'[::]:{port}')
+    await server.start()
+
+    print(f"Server started, listening on {port}")
+
+    # keep the server running
+    await server.wait_for_termination()
+
+if __name__ == "__main__":
+    asyncio.run(serve())
